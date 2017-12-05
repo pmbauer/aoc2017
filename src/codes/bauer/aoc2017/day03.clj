999c644f5f9e49d61cb5b717816992941f642064
Paul Bauer <paul@bauer.codes>
2017-12-05 10:01:00 -0500

    day 3 as sequence

--- src/codes/bauer/aoc2017/day03.clj
+++ src/codes/bauer/aoc2017/day03.clj
@@ -1,32 +1,47 @@
 (ns codes.bauer.aoc2017.day03)
 
-;; part1
-(defn distance [x]
+(defn square-spiral
+  "lazy seq of square spiral sums and coordinates:
+  ({:coord [x0,y0] :val v0} ...)"
+  []
   (letfn [(move [[x y] [dir]]
             (case dir
               :north [x (inc y)]
               :south [x (dec y)]
               :east  [(inc x) y]
-              :west  [(dec x) y]))]
-    (loop [i 1
-           loc [0 0]
-           segment-pos 0
-           distances (drop 2 (interleave (range) (range)))
-           turns (cycle [:south :east :north :west])]
-      (cond
-        ;; found target, compute distance
-        (= i x) (transduce (map #(Math/abs %)) + loc)
-        ;; execute turn
-        (= 0 segment-pos) (recur i loc (first distances) (rest distances) (rest turns))
-        ;; move along segment
-        :else (recur (inc i) (move loc turns) (dec segment-pos) distances turns)))))
+              :west  [(dec x) y]))
+          (adjacents [[x y]]
+            [[x       (inc y)]
+             [x       (dec y)]
+             [(inc x) y]
+             [(inc x) (inc y)]
+             [(inc x) (dec y)]
+             [(dec x) y]
+             [(dec x) (inc y)]
+             [(dec x) (dec y)]])
+          (spiral [coord grid segment-pos distances turns]
+            (if (= 0 segment-pos)
+              ;; execute turn
+              (recur coord grid (first distances) (rest distances) (rest turns))
+              (lazy-seq
+               (cons {:coord coord, :val (grid coord)}
+                     (let [coord (move coord turns)
+                           val (transduce (map #(get grid % 0)) +' (adjacents coord))
+                           grid (assoc grid coord val)]
+                       (spiral coord grid (dec segment-pos) distances turns))))))]
+    (let [coord0 [0 0]
+          grid {coord0 1}
+          distances (drop 2 (interleave (range) (range)))
+          turns (cycle [:south :east :north :west])]
+      (spiral coord0 grid 0 distances turns))))
+
+;; part1
+(defn distance [n]
+  (let [[x y] (:coord (nth (square-spiral) (dec n)))]
+    (+ (Math/abs x) (Math/abs y))))
 
 (distance 1)
 ;; => 0
-(distance 12)
-;; => 3
-(distance 23)
-;; => 2
 (distance 1024)
 ;; => 31
 (distance 325489)
@@ -35,35 +50,7 @@
 
 ;; part2
 (defn find-greater [x]
-  (letfn [(move [[x y] [dir]]
-            (case dir
-              :north [x (inc y)]
-              :south [x (dec y)]
-              :east  [(inc x) y]
-              :west  [(dec x) y]))]
-    (loop [loc [0 0]
-           grid {loc 1}
-           segment-pos 0
-           distances (drop 2 (interleave (range) (range)))
-           turns (cycle [:south :east :north :west])]
-      (let [val (grid loc)]
-        (cond
-          ;; found target, return val
-          (> val x) val
-          ;; execute turn
-          (= 0 segment-pos) (recur loc grid (first distances) (rest distances) (rest turns))
-          ;; move along segment, compute next value
-          :else (let [[x y :as loc] (move loc turns)
-                      val (transduce (map #(get grid % 0)) + [[x       (inc y)]
-                                                              [x       (dec y)]
-                                                              [(inc x) y]
-                                                              [(inc x) (inc y)]
-                                                              [(inc x) (dec y)]
-                                                              [(dec x) y]
-                                                              [(dec x) (inc y)]
-                                                              [(dec x) (dec y)]])
-                      grid (assoc grid loc val)]
-                  (recur loc grid (dec segment-pos) distances turns)))))))
+  (->> (square-spiral) (drop-while #(<= (:val %) x)) first :val))
 
 (find-greater 1)
 ;; => 2
